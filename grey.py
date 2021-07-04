617b83f49980d87fd9e80f7ba0c12626e5fadde9
Riddhiman16 <84495392+Riddhiman16@users.noreply.github.com>
2021-07-04 17:21:31 +0530

    Add files via upload
    it is a python program to convert a coloured image to black and white(grayscale) format.

---
+++ grey.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+# read the image
+image = cv2.imread('mw.jpg')
+
+# convert to RGB
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+# reshape the image to a 2D array of pixels and 3 color values (RGB)
+p = image.reshape((-1, 3))
+w=len(p)
+
+for i in range(0,w-1):
+    h=(int(p[i][0])+int(p[i][1])+int(p[i][2]))/3
+    p[i][0] = h
+    p[i][1] = h
+    p[i][2] = h
+
+
+s = p.reshape(image.shape)
+
+# show the image
+plt.imshow(s)
+plt.show()
